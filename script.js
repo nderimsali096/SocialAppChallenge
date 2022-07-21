f23cb6e01829e343926c88e47b2e541ba308e0df
nderimsali1 <nderim.sali@mantaro.partners>
2022-07-21 11:35:13 +0200

    c

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-'use strict'
+"use strict";
 
 let index = 0;
-let postObject = {}
-let buttonLoad = document.getElementById('btn');
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
+let postObject = {};
+let buttonLoad = document.getElementById("btn");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
 
 async function getPosts() {
-    let url = '/data.json';
-    try {
-        let res = await fetch(url);
-        return await res.json();
-    } catch (error) {
-        console.log(error);
-    }
+  let url = "/data.json";
+  try {
+    let res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function renderPosts() {
-    let posts = await getPosts();
-    Object.assign(postObject, posts[0])
-    let html = '';
-    let counter = 0;
-    for (let i = index;i < posts.length;i++){
-        if (counter === 4) {
-            break
-        }
-        let htmlSegment = `<div id="postClicked" class="post">
+  let posts = await getPosts();
+  Object.assign(postObject, posts[0]);
+  let html = "";
+  let counter = 0;
+  for (let i = index; i < posts.length; i++) {
+    if (counter === 4) {
+      break;
+    }
+    let htmlSegment = `<div id="postClicked" class="post">
         <header id="postClicked" class="header">
             <div id="postClicked" class="header--info">
                 <img id="postClicked" 
@@ -59,47 +59,45 @@ async function renderPosts() {
             </div>
         </div>
         </div>`;
-        html += htmlSegment;
-        index++;
-        counter++;
-    }
-    let container = document.getElementById('posts');
-    container.innerHTML += html;
-    if (index === posts.length) {
-        buttonLoad.style.display = "none";
-    }
-    if (index > 4) {
-        window.scrollTo({
-            left: 0,
-            top: document.body.scrollHeight,
-            behavior: 'smooth',
-        })
-    }
+    html += htmlSegment;
+    index++;
+    counter++;
+  }
+  let container = document.getElementById("posts");
+  container.innerHTML += html;
+  if (index === posts.length) {
+    buttonLoad.style.display = "none";
+  }
+  if (index > 4) {
+    window.scrollTo({
+      left: 0,
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  }
 }
 
 renderPosts();
 
-buttonLoad.addEventListener('click', () => {
-    renderPosts();
-})
+buttonLoad.addEventListener("click", () => {
+  renderPosts();
+});
 
-document.body.addEventListener( 'click', function ( event ) {
-    if( event.target.id === 'postClicked' ) {
-        showModal();
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    };
-  });
-
-  
-  
-  const closeTheModal = function () {
-      modal.classList.add('hidden');
-      overlay.classList.add('hidden');
+document.body.addEventListener("click", function (event) {
+  if (event.target.id === "postClicked") {
+    showModal();
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
   }
+});
 
-  function showModal() {
-    let html = `<div class="inner-modal">
+const closeTheModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+function showModal() {
+  let html = `<div class="inner-modal">
     <img class="img-modal" src="${postObject.image}" />
         <div class="left-side">
             <header class="header-modal">
@@ -130,18 +128,17 @@ document.body.addEventListener( 'click', function ( event ) {
             </div>
         </div>
     </div>`;
-    let modalContainer = document.getElementById('modal');
-    modalContainer.innerHTML = html;
-  }
+  let modalContainer = document.getElementById("modal");
+  modalContainer.innerHTML = html;
+}
 
-  
-  btnCloseModal.addEventListener('click',closeTheModal);
-  overlay.addEventListener('click',closeTheModal);
-  
-  document.addEventListener('keydown',function(event){
-      if(event.key === 'Escape'){
-          if(!modal.classList.contains('hidden')){
-              closeTheModal();
-          }
-      }
-  });
+btnCloseModal.addEventListener("click", closeTheModal);
+overlay.addEventListener("click", closeTheModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeTheModal();
+    }
+  }
+});
