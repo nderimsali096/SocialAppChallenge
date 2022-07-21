7c133950bee3ce523c4d70a21ece567133b117d6
nderimsali1 <nderim.sali@mantaro.partners>
2022-07-21 11:54:57 +0200

    n

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ async function getPosts() {
     console.log(error);
   }
 }
-// adding some comments
+// adding some comments 1
 async function renderPosts() {
   let posts = await getPosts();
   Object.assign(postObject, posts[0]);
